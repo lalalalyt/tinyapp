bec8343084347ecc8a8542eaac379eddc34547b6
lalalalyt <lynneli318@gmail.com>
2022-02-23 10:50:15 -0500

    add delete bottom

--- express_server.js
+++ express_server.js
@@ -66,3 +66,9 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL);
 });
+
+app.post("/urls/:shortURL/delete", (req,res)=>{
+  const shortURL = req.params.shortURL
+  delete urlDatabase[shortURL]
+  res.redirect("/urls")
+})
